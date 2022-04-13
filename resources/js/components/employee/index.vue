e804016c5dd6384dbceb907efd406fdfe4921e03
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2022-04-13 18:31:17 +0000

    Read data and search part 1

--- resources/js/components/employee/index.vue
+++ resources/js/components/employee/index.vue
@@ -1,48 +1,138 @@
-<template>
-    <div>
-        <div class="row justify-content-center">
-            <router-link to="/store-employee" class="btn btn-primary"> Ajouter un employés </router-link>
-        </div>
-        <div class="row justify-content-center">
-            <div class="col-xl-10 col-lg-12 col-md-9">
-                <div class="card shadow-sm my-5">
-                    <div class="card-body p-0">
-                        <div class="row">
-                            <div class="col-lg-12">
-                                <div class="login-form">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Ajouter un employé</h1>
-                                    </div>
 
-                                </div>
-                            </div>
-                        </div>
+
+<template>
+
+    <div>
+
+        <div class="row">
+            <router-link to="/store-employee" class="btn btn-primary">Ajouter un Employé </router-link>
+
+        </div>
+        <br>
+        <input type="text" v-model="searchTerm" class="form-control" style="width: 300px;" placeholder="rechercher ici">
+
+
+        <br>
+
+        <div class="row">
+            <div class="col-lg-12 mb-4">
+                <!-- Simple Tables -->
+                <div class="card">
+                    <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+                        <h6 class="m-0 font-weight-bold text-primary">Liste des employés</h6>
                     </div>
+                    <div class="table-responsive">
+                        <table class="table align-items-center table-flush">
+                            <thead class="thead-light">
+                            <tr>
+                                <th>Nom</th>
+                                <th>Photo</th>
+                                <th>Téléphone</th>
+                                <th>Salaire</th>
+                                <th>Date d'embauche</th>
+                                <th>Action</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            <tr v-for="employee in filtersearch" :key="employee.id">
+                                <td> {{ employee.name }} </td>
+                                <td><img :src="employee.photo" id="em_photo"></td>
+                                <td>{{ employee.phone }}</td>
+                                <td>{{ employee.sallery }}</td>
+                                <td>{{ employee.joining_date }}</td>
+                                <td>
+                                    <router-link :to="{name: 'edit-employee', params:{id:employee.id}}" class="btn btn-sm btn-primary">Editer</router-link>
+
+                                    <a @click="deleteEmployee(employee.id)" class="btn btn-sm btn-danger"><font color="#ffffff">Supprimer</font></a>
+                                </td>
+                            </tr>
+
+                            </tbody>
+                        </table>
+                    </div>
+                    <div class="card-footer"></div>
                 </div>
             </div>
         </div>
+        <!--Row-->
+
+
+
     </div>
+
+
 </template>
 
-<script>
+
+
+<script type="text/javascript">
+
 export default {
-    name: "register",
-    created() {
-        if (!User.loggedIn()){
+    created(){
+        if (!User.loggedIn()) {
             this.$router.push({name: '/'})
+        }
+        this.allEmployee();
+    },
+    data(){
+        return{
+            employees:[],
+            searchTerm:''
+        }
+    },
+    computed:{
+        filtersearch(){
+            return this.employees.filter(employee => {
+                return employee.name.match(this.searchTerm)
+            })
         }
     },
 
-    //name: "login",
-    data(){
-        return{
-        }
-    },
-    methods:{
-    }
+    methods: {
+        allEmployee() {
+            axios.get('/api/employee/')
+                .then(({data}) => (this.employees = data))
+                .catch()
+        },
+            // deleteEmployee(id){
+            //     Swal.fire({
+            //         title: 'Are you sure?',
+            //         text: "You won't be able to revert this!",
+            //         icon: 'warning',
+            //         showCancelButton: true,
+            //         confirmButtonColor: '#3085d6',
+            //         cancelButtonColor: '#d33',
+            //         confirmButtonText: 'Yes, delete it!'
+            //     }).then((result) => {
+            //         if (result.value) {
+            //             axios.delete('/api/employee/'+id)
+            //                 .then(() => {
+            //                     this.employees = this.employees.filter(employee => {
+            //                         return employee.id != id
+            //                     })
+            //                 })
+            //                 .catch(() => {
+            //                     this.$router.push({name: 'employee'})
+            //                 })
+            //             Swal.fire(
+            //                 'Deleted!',
+            //                 'Your file has been deleted.',
+            //                 'success'
+            //             )
+            //         }
+            //     })
+            //
+            // }
+
+        },
+
 }
 </script>
 
-<style scoped>
 
+<style type="text/css">
+#em_photo{
+    height: 40px;
+    width: 40px;
+}
 </style>
