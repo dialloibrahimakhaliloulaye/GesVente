33da12f27fd66e67c708328832c037aac72d8bd5
Ibrahima Khaliloulaye DIALLO <58261204+dialloibrahimakhaliloulaye@users.noreply.github.com>
2022-04-16 00:36:02 +0000

    Employee salary part 2

--- app/Http/Controllers/Api/SalaryController.php
+++ app/Http/Controllers/Api/SalaryController.php
@@ -4,8 +4,29 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class SalaryController extends Controller
 {
-    //
+    public function Paid(Request $request,$id)
+    {
+        $ValidateData = $request->validate([
+            'salary_month' => 'required',
+        ]);
+
+        $month = $request->salary_month;
+        $check = DB::table('salaries')->where('employee_id',$id)->where('salary_month',$month)->first();
+        if ($check) {
+            return response()->json('Salary Alrady Paid');
+        }else{
+            $data = array();
+            $data['employee_id'] = $id;
+            $data['amount'] = $request->sallery;
+            $data['salary_date'] = date('d/m/Y');
+            $data['salary_month'] = $month;
+            $data['salary_year'] = date('Y');
+            DB::table('salaries')->insert($data);
+        }
+
+    }
 }
